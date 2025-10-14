240a8340a54062d44f0325bdb7f83a8f11e43888
Sebastian Reconco <sebastianreconco@gmail.com>
2025-10-13 20:04:03 -0400

    vite and tailwind setup added, changed some docker stuff to reflect that

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// to run docker
+// docker compose up -d
+// then go to http://localhost:5173 for frontend
+// http://localhost:8000/health for API
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <h1 className="font-bold text-blue-400">does tailwind work?</h1>
+    <h1 className="font-bold text-blue-400">if this text is blue, tailwind works :D</h1>
   </StrictMode>,
 )
