5512e73222dc4ae240a888ab704a456c2451792f
Katerina Garkova <katerina.garkova@gmail.com>
2025-10-29 13:35:10 -0400

    Implemented Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Play from './pages/Play'
 import Leaderboard from './pages/Leaderboard'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState('login')
+
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'login':
+        return <Login onNavigateToSignUp={() => setCurrentPage('signup')} />
+      case 'signup':
+        return <SignUp onNavigateToLogin={() => setCurrentPage('login')} />
+      case 'dashboard':
+        return <Dashboard />
+      case 'play':
+        return <Play />
+      case 'leaderboard':
+        return <Leaderboard />
+      default:
+        return <Login onNavigateToSignUp={() => setCurrentPage('signup')} />
+    }
+  }
 
   return (
     <div>
